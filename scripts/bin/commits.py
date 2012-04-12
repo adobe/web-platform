131dc7a599fcb3c4af4283213ec0543bb928ff4d
Ethan Malasky <ethan@malasky.net>
2012-04-12 10:37:48 -0700

    more fun code to call git from python

--- scripts/bin/commits.py
+++ scripts/bin/commits.py
@@ -6,6 +6,8 @@ import re
 import StringIO
 
 class GitCommit:
+	all_commits = []
+
 	def __init__(self):
 		pass
 	
@@ -13,8 +15,22 @@ class GitCommit:
 	def parse_stream( stream ):
 
 		cc = None # Current Commit
-		while stream.next():
-			print r
+		while True:
+			try:
+				r = stream.next()
+			except StopIteration:
+				break
+			if r:
+				print r
+				commit_match = re.match("^commit (\w*)$", r)
+				if commit_match:
+					print "match = ", m.group(1)
+					if cc:
+						all_commits += cc
+						cc = GitCommit()
+					else if
+    # e.g. Patch by Eric Seidel <eric@webkit.org> on 2011-09-15
+    patch_by_regexp = r'^Patch by (?P<name>.+?)\s+<(?P<email>[^<>]+)> on (?P<date>\d{4}-\d{2}-\d{2})$'
 
 
 def git_log( since=None, until=None ):
@@ -29,7 +45,7 @@ def git_log( since=None, until=None ):
 	proc = Popen( [cmd], shell=True, stdout=PIPE )
 	f = StringIO.StringIO( proc.communicate()[0] )
 
-
+	return GitCommit.parse_stream(f)
 
 full = git_log();
 
