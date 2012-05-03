ff16a5c5915065cf3af10094eff326fa3b9ace26
Alexandru Chiculita <achicu@adobe.com>
2012-05-03 11:52:53 -0700

    Updated year from 2011 to 2012

--- css-shaders/resources/template-style.css
+++ css-shaders/resources/template-style.css
@@ -1,5 +1,5 @@
 /*
-Copyright 2011 Adobe Systems, incorporated
+Copyright 2012 Adobe Systems, Incorporated
 This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License http://creativecommons.org/licenses/by-nc-sa/3.0/ . 
 Permissions beyond the scope of this license, pertaining to the examples of code included within this work are available at Adobe http://www.adobe.com/communities/guidelines/ccplus/commercialcode_plus_permission.html .
 */
