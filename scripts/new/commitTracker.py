9875294beea89010ccfa00ae6d392a354e7d1320
Bem Jones-Bey <bjonesbe@adobe.com>
2012-07-23 15:30:07 -0700

    Commit tracker now has basic functionality.

--- scripts/new/commitTracker.py
+++ scripts/new/commitTracker.py
@@ -6,10 +6,9 @@
 # FIXME all of the following should be in a config file and set via command
 # line args
 
+verbose = False
+
 # Date to start grabbing commits at.
-# FIXME this must be of this form, or it will not work. Need to get a proper
-# date parsing library. Or should this just be automatically generated as back
-# to the beginning of the current year?
 since='01/01/2012' 
 
 # repository
@@ -17,35 +16,79 @@ since='01/01/2012'
 # to be set
 repositoryRoot = '/Users/bjonesbe/Code/WebKit'
 
-# default ref
-branchHead = 'refs/remotes/origin/master'
-
 # dictionary of people who's contributions we are looking for
 # name -> email address
-people = [
-    'Bear Travis': '', # FIXME I need an email
+people = {
+    'Bear Travis': '',
     'Mihnea': 'mihnea@adobe.com',
     'Chiculita': 'achicu@adobe.com',
-    'Raul Hudea': '', # FIXME I need an email
-    'Max Vujovic': '', # FIXME I need an email
-    'Hans Muller': '', # FIXME I need an email
-    'Ethan Malasky': '', # FIXME I need an email
-    'Arno Gourdol': '', # FIXME I need an email
-    'Alan Stearns': '', # FIXME I need an email
-    'Larry McLister': '', # FIXME I need an email
-    'Jacob Goldstein': '', # FIXME I need an email
+    'Raul Hudea': '',
+    'Max Vujovic': '',
+    'Hans Muller': '',
+    'Ethan Malasky': '',
+    'Arno Gourdol': '',
+    'Alan Stearns': '',
+    'Larry McLister': '',
+    'Jacob Goldstein': '',
     'Dirk Schulze': 'krit@webkit.org',
-    'Rebecca Hauck': '', # FIXME I need an email
-    'Flex Mobile': '', # FIXME I need an email
-    'David Alcala': '', # FIXME I need an email
-    'Victor Carbune': '', # FIXME I need an email
-    'Mihai Balan': '', # FIXME I need an email
+    'Rebecca Hauck': '',
+    'Flex Mobile': '',
+    'David Alcala': '',
+    'Victor Carbune': '',
+    'Mihai Balan': '',
     'Bem Jones-Bey': 'bjonesbe@adobe.com'
-]
+}
 
 ################################################################################
 
-from dulwich.repo import Repo
+import os
+import re
+from subprocess import Popen,PIPE,check_call
+import sys
 
-repo = Repo(repositoryRoot)
+def filterAndEscape(l):
+    return [ re.escape(i) for i in l if len(i) > 0 ]
 
+peopleMatcher = re.compile('|'.join(filterAndEscape(people.keys())) + '|' + '|'.join(filterAndEscape(people.values())))
+
+class Counter(object):
+    def __init__(self, data):
+        self.data = data
+        self.count = 0
+        self._currentCommit = 'NONE'
+
+    def start(self):
+        self._nextCommit()
+        for line in self.data:
+            if line.startswith('Author'):
+                if self._lineHasPerson(line):
+                    if verbose:
+                        print line
+                    self.count += 1
+                    self._nextCommit()
+            elif line.strip().startswith('Patch by'):
+                if self._lineHasPerson(line):
+                    if verbose:
+                        print line
+                    self.count += 1
+                    self._nextCommit()
+
+    def _nextCommit(self):
+        for line in self.data:
+            if line.startswith('commit'):
+                if verbose:
+                    print line
+                return
+
+    def _lineHasPerson(self, line):
+        return peopleMatcher.search(line)
+
+os.chdir(repositoryRoot)
+print("Fetching updates")
+check_call(['git', 'fetch', 'origin'])
+
+log = Popen(['git', 'log', 'origin/master', '--since="{0}"'.format(since)], stdout=PIPE)
+counter = Counter(log.stdout)
+counter.start()
+
+print('Counted {0} commits since {1}'.format(counter.count, since))
