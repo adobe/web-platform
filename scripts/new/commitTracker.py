6820d1ab57d5c3ad715680f7b4c7d2d93100601e
Bem Jones-Bey <bjonesbe@adobe.com>
2012-07-24 11:09:59 -0700

    Get config file working, and add basic sample config

--- scripts/new/commitTracker.py
+++ scripts/new/commitTracker.py
@@ -15,6 +15,8 @@ class Config(object):
     def __init__(self):
         self.set_defaults()
         self.parse_args()
+        self.read_config()
+        self.set_config()
 
     def set_defaults(self):
         home = os.getenv('HOME')
@@ -23,7 +25,7 @@ class Config(object):
             # configuration file
             self.config_file = home + os.sep + '.committrackerrc'
             # repository
-            self.repository_root = home + os.sep + 'Code' + os.sep + 'WebKit'
+            self.repository_root = home + os.sep + 'WebKit'
         else:
             self.config_file = None
             self.repository_root = None
@@ -32,39 +34,55 @@ class Config(object):
         self.do_fetch = True
         # Date to start grabbing commits at.
         self.since = '01/01/{0}'.format(now.tm_year)
-        # FIXME this must move to the config file!
+        # This should be set in the config file in the "People" section!
         # dictionary of people who's contributions we are looking for
         # name -> email address
-        self.people = {
-            'Bear Travis': '',
-            'Mihnea': 'mihnea@adobe.com',
-            'Chiculita': 'achicu@adobe.com',
-            'Raul Hudea': '',
-            'Max Vujovic': '',
-            'Hans Muller': '',
-            'Ethan Malasky': '',
-            'Arno Gourdol': '',
-            'Alan Stearns': '',
-            'Larry McLister': '',
-            'Jacob Goldstein': '',
-            'Dirk Schulze': 'krit@webkit.org',
-            'Rebecca Hauck': '',
-            'Flex Mobile': '',
-            'David Alcala': '',
-            'Victor Carbune': '',
-            'Mihai Balan': '',
-            'Bem Jones-Bey': 'bjonesbe@adobe.com'
-        }
-        self.people_matcher = re.compile(self.people_regexp())
+        self.people = { }
 
     def parse_args(self):
         parser = argparse.ArgumentParser(description='Count commits by the Adobe Web Platform Team')
-        parser.add_argument('--config', default=self.config_file, help='Path to config file')
+        parser.add_argument('--config', default=None, help='Path to config file, default is {0}'.format(self.config_file))
         parser.add_argument('--verbose', action='store_true', help='Turn on verbose mode')
         parser.add_argument('--no-fetch', dest='no_fetch', action='store_true', help="Don't fetch from origin before counting")
-        parser.add_argument('--since', default=self.since, help='Start date for counting. Defaults to Jan 1st of the current year.')
-        parser.add_argument('--repo', dest='repository_root', default=self.repository_root, help='Path to WebKit git repository')
+        parser.add_argument('--since', default=None, help='Start date for counting. Defaults to Jan 1st of the current year.')
+        parser.add_argument('--repo', dest='repository_root', default=None, help='Path to WebKit git repository')
         self.args = parser.parse_args()
+
+    def read_config(self):
+        self.file = SafeConfigParser(allow_no_value=True)
+        if self.args.config:
+            self.config_file = self.args.config
+            with open(self.config_file) as fp:
+                self.file.readfp(fp, self.config_file)
+        else:
+            self.file.read(self.config_file)
+
+    def set_config(self):
+        if self.args.verbose:
+            self.verbose = True
+        elif self.file.has_option('Options', 'verbose'):
+            self.verbose = self.file.get_boolean('Options', 'verbose')
+
+        if self.args.no_fetch:
+            self.do_fetch = False
+        elif self.file.has_option('Options', 'do_fetch'):
+            self.do_fetch = self.file.get_boolean('Options', 'do_fetch')
+
+        if self.args.since:
+            self.since = self.args.since
+        elif self.file.has_option('Options', 'since'):
+            self.since = self.file.get('Options', 'since')
+
+        if self.args.repository_root:
+            self.repository_root = self.args.repostitory_root
+        elif self.file.has_option('Options', 'repository_root'):
+            self.repository_root = self.file.get('Options', 'repository_root')
+
+        if self.file.has_section('People'):
+            self.people = { person[0] : person[1] for person in self.file.items('People') }
+
+        # This is case insensitive because ConfigParser throws away the case of all of it's keys.
+        self.people_matcher = re.compile(self.people_regexp(), re.IGNORECASE)
 
     def people_regexp(self):
         def helper(l):
