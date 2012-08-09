190f6bb3ebf90438685f12ec6c873e02d822cbf7
Ethan Malasky <ethan@malasky.net>
2012-08-09 10:09:14 -0500

    better label layout. still missing start and end labels.

--- presentations/commit-graph/commit-graph.js
+++ presentations/commit-graph/commit-graph.js
@@ -2,15 +2,32 @@ Raphael.fn.drawGrid = function (x, y, w, h, wv, hv, wl, hl, color) {
 	// remember the grid
 	this.gridLocation = { x:x, y:y, w:w, h:h, wv:wv, hv:hv };
 
+    var wlabels = null;
+    if ( wl ){
+        wlabels = this.set();
+    }
     color = color || "#000";
-    var path = ["M", Math.round(x) + .5, Math.round(y) + .5, "L", Math.round(x + w) + .5, Math.round(y) + .5, Math.round(x + w) + .5, Math.round(y + h) + .5, Math.round(x) + .5, Math.round(y + h) + .5, Math.round(x) + .5, Math.round(y) + .5],
+    var path = ["M"
+                , Math.round(x) + .5, Math.round(y) + .5 /* TL */
+            , "L"
+                , Math.round(x + w) + .5, Math.round(y) + .5 /* TR */
+                , Math.round(x + w) + .5, Math.round(y + h) + .5 /* BR */
+                , Math.round(x) + .5, Math.round(y + h) + .5 /* BL */
+                , Math.round(x) + .5, Math.round(y) + .5], /* TL, again */
         rowHeight = h / hv,
         columnWidth = w / wv;
     for (var i = 1; i < hv; i++) {
-        path = path.concat(["M", Math.round(x) + .5, Math.round(y + i * rowHeight) + .5, "H", Math.round(x + w) + .5]);
+       path = path.concat(["M", Math.round(x) + .5, Math.round(y + i * rowHeight) + .5
+                         , "H", Math.round(x + w) + .5]);
     }
     for (i = 1; i < wv; i++) {
         path = path.concat(["M", Math.round(x + i * columnWidth) + .5, Math.round(y) + .5, "V", Math.round(y + h) + .5]);
+        if ( wl[i] ) {
+            var text = this.text( x + i * columnWidth + .5, 
+                    Math.round(y + h) + (.5 * rowHeight) + .5
+                    , wl[i]).attr( this.textattr );
+            wlabels.push( );
+        }
     }
     return this.path(path.join(",")).attr({stroke: color});
 };
@@ -52,7 +69,9 @@ window.onload = function(){
 
     labels = [], data = [];
     for (var i = 0; i < commitdata.length; i++) {
-        labels.push(commitdata[i].label);
+        var d = new Date( commitdata[i].label );
+        var l = d.getMonth()+1+'/'+(d.getDate()+1)+'/'+d.getFullYear();
+        labels.push(l);
         data.push(commitdata[i].value);
     };
 
@@ -66,7 +85,7 @@ window.onload = function(){
         columncount  = 52,
         rowincrement = 50,
         r = Raphael("commit-holder", width, height),
-        txt = {font: '12px Ubuntu, Arial', fill: "#fff"},
+        textattr = {font: '10px Ubuntu, Arial', fill: color},
         txt1 = {font: '10px Ubuntu, Arial', fill: "#fff"},
         txt2 = {font: '12px Ubuntu, Arial', fill: "#000"},
         X = (width - leftgutter) / columncount,
@@ -74,6 +93,8 @@ window.onload = function(){
         committarget = 300,
         max = Math.max( max, committarget ),
         Y = (height - bottomgutter - topgutter) / max;
+
+        r.textattr = textattr;
 
     function drawBaseline( startval, endval, attr ){
         var path = r.path()
@@ -91,9 +112,9 @@ window.onload = function(){
                 , topgutter + .5  /* y */
                 , width - leftgutter - X /* w */
                 , height - topgutter - bottomgutter /* h */
-                , 12 /* wv */
+                , 11 /* wv */
                 , Math.floor( max/rowincrement)  /* hv */
-                , null /* wl */
+                , ['1/1','2/1','3/1','4/1','5/1','6/1','7/1','8/1','9/1','10/1','11/1','12/1'] /* wl */
                 , null /* hl */
                 , "#000" /* color */
                 );
