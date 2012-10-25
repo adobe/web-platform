bc25c9a077b0e04821f4f4c502cdc4cea1343f31
Bear Travis <betravis@adobe.com>
2012-10-24 17:44:33 -0700

    cleaning up shape-test.js and sample-template.css

--- samples/css-exclusions/resources/js/shape-test.js
+++ samples/css-exclusions/resources/js/shape-test.js
@@ -1,47 +1,31 @@
-!function(){  
-    
-    function CSSExclusions(){}
-    
-    // basic feature detection 
-    CSSExclusions.prototype.isSupported = (function(prop){
-
-        var el = document.createElement("div"),
-            style = el.style;
-
-        if (!el){
-            return false
-        }
+!function(){
+    var isEnabled = function() {
+        var el = document.createElement('div');
+        if (!el)
+            return;
 
         // intentional bias towards -webkit because the prototype uses 
         // code under prefixed notation that is still being debated in the W3C
-        style.setProperty("-webkit-shape-inside", "rectangle(0, 0, 100%, 100%)");
-        return (style.getPropertyValue("-webkit-shape-inside") != null);
-    })()    
-
-    var CSSExclusions = new CSSExclusions()
-
-    var checkSupport = function(){
-        if (CSSExclusions.isSupported){
-            return 
-        }
-        else{
-            var target = document.querySelector('article section') 
-
-            if (target){
-                target.innerHTML = createWarning()
-            }
-        }
+        el.style.setProperty("-webkit-shape-inside", "rectangle(0, 0, 100%, 100%)");
+        return (el.style.getPropertyValue("-webkit-shape-inside") != null);
     }
 
-    var createWarning = function(){
-        var h = [],
-            p = function(){ h.push.apply(h, arguments) }
-            
-        p('<p class="unsupported error">')
-            p('<strong>Warning:</strong> You need to use the Chrome Canary browser with experimental WebKit features enabled to see this example working correctly. See <a href="../index.html#browser-support">here</a> for more info.')
-        p('</p>')
+    var checkSupport = function() {
+        if (isEnabled())
+            return;
+        createWarning();
+    }
 
-        return h.join('')
+    var createWarning = function() {
+        var warning = document.createElement('div');
+        warning.setAttribute('class', 'error');
+
+        var content = [];
+        content.push('<p>');
+        content.push('<strong>Warning:</strong> You need to use the Chrome Canary browser with experimental WebKit features enabled to see this example working correctly. See <a href="../index.html#browser-support">here</a> for more info.')
+        content.push('</p>');
+        warning.innerHTML = content.join('');
+        document.body.insertBefore(warning, document.body.children[0]);
     }
 
     document.addEventListener("DOMContentLoaded", checkSupport)
